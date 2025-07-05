d0da89c2b77816aac36246c5327677c097a6ad04
phoityne.hs@gmail.com <phoityne.hs@gmail.com>
2025-07-05 09:01:43 +0900

    release-0.0.5.0.

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 cabal update
 cabal clean
 cabal configure
-cabal build --enable-executable-static
+# cabal build --enable-executable-static
+cabal build
 cabal install --overwrite-policy=always
 cabal sdist
 # cabal run pty-mcp-server -- -y ./configs/pty-mcp-server.yaml 
